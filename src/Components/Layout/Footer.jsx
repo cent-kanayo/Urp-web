4cf392ca6afd39ea43d55d5e1fbe6781f5b80551
cent kanayo <centkanayo@gmail.com>
2023-10-28 22:43:22 +0100

    Feat: Adding responsiveness to news page

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -11,8 +11,10 @@ const Footer = () => {
           <div className="flex lg:flex-row justify-center gap-[35px] lg:gap-[40px]">
             <div className="flex flex-col lg:flex-row justify-center gap-[35px] lg:gap-[40px]">
               <div className="capitalize">
-                <h3 className="text-[20px] font-medium mb-[16px]">Menu</h3>
-                <ul className="text-[14px] font-[300] text-[#FDFFFD] flex flex-col gap-[8px]">
+                <h3 className="text-[14px] lg:text-[20px] font-medium mb-[16px]">
+                  Menu
+                </h3>
+                <ul className="text-[12px] lg:text-[14px] font-[300] text-[#FDFFFD] flex flex-col gap-[8px]">
                   <li>Home</li>
                   <li>about</li>
                   <li>Academics</li>
@@ -21,27 +23,31 @@ const Footer = () => {
                 </ul>
               </div>
               <div className="capitalize">
-                <h3 className="text-[20px] font-medium mb-[16px]">contact</h3>
-                <ul className="text-[14px] font-[300] text-[#FDFFFD]  flex flex-col gap-[8px]">
+                <h3 className="text-[14px] lg:text-[20px] font-medium mb-[16px]">
+                  contact
+                </h3>
+                <ul className="text-[12px] lg:text-[14px] font-[300] text-[#FDFFFD]  flex flex-col gap-[8px] whitespace-nowrap">
                   <li>mail</li>
                   <li>phone number</li>
                 </ul>
               </div>
             </div>
             <div className="capitalize">
-              <h3 className="text-[20px] font-medium">university of lagos</h3>
+              <h3 className="text-[14px] lg:text-[20px] font-medium">
+                university of lagos
+              </h3>
               <p className="text-[14px] font-[300] text-[#FDFFFD]  mb-[8px]">
                 faculty of environmental sciences.
               </p>
               <div className="mb-3">
-                <p className="text-[14px] font-[300] text-[#FDFFFD]">
+                <p className="text-[12px] lg:text-[14px] font-[300] text-[#FDFFFD]">
                   akoka rd, yaba, lagos nigeria
                 </p>
-                <p className="text-[14px] font-[300] text-[#FDFFFD]">
+                <p className="text-[12px] lg:text-[14px] font-[300] text-[#FDFFFD]">
                   open monday - friday from 8:00am to 4:00pm
                 </p>
               </div>
-              <p className="text-[#EAA966] text-[14px]">
+              <p className="text-[#EAA966] text-[12px] lg:text-[14px]">
                 visit university website
               </p>
             </div>
@@ -49,7 +55,7 @@ const Footer = () => {
         </header>
       </section>
       <section className="h-[75px] flex justify-center items-center bg-white shadow-md py-4 lg:py-0">
-        <p className="text-center">
+        <p className="text-center text-[10px] lg:text-[14px]">
           Copyright &copy; {new Date().getFullYear()} department of urban and
           regional planning
         </p>
