dab9fc4cb63895ace4fbec5d776500b7edeb8033
cent kanayo <centkanayo@gmail.com>
2024-02-12 14:45:43 +0100

    Making updates to files

--- Client/src/Components/Pages/Faq.jsx
+++ Client/src/Components/Pages/Faq.jsx
@@ -16,12 +16,20 @@ const Faq = () => {
   const [arrowUp3, setArrowUp3] = useState(false);
   const [arrowUp4, setArrowUp4] = useState(false);
   const [arrowUp5, setArrowUp5] = useState(false);
+  const [arrowUp6, setArrowUp6] = useState(false);
+  const [arrowUp7, setArrowUp7] = useState(false);
+  const [arrowUp8, setArrowUp8] = useState(false);
+  const [arrowUp9, setArrowUp9] = useState(false);
 
   const ref1 = useRef(null);
   const ref2 = useRef(null);
   const ref3 = useRef(null);
   const ref4 = useRef(null);
   const ref5 = useRef(null);
+  const ref6 = useRef(null);
+  const ref7 = useRef(null);
+  const ref8 = useRef(null);
+  const ref9 = useRef(null);
 
   const onDropdownClicked = (tarket) => {
     if (tarket === '1') {
@@ -30,11 +38,19 @@ const Faq = () => {
       ref3.current.classList.remove('programs-show');
       ref4.current.classList.remove('programs-show');
       ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
       setArrowUp1((prev) => !prev);
       setArrowUp2(false);
       setArrowUp3(false);
       setArrowUp4(false);
       setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
     }
     if (tarket === '2') {
       ref2.current.classList.toggle('programs-show');
@@ -42,11 +58,19 @@ const Faq = () => {
       ref3.current.classList.remove('programs-show');
       ref4.current.classList.remove('programs-show');
       ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
       setArrowUp2((prev) => !prev);
       setArrowUp1(false);
       setArrowUp3(false);
       setArrowUp4(false);
       setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
     }
     if (tarket === '3') {
       ref3.current.classList.toggle('programs-show');
@@ -54,11 +78,19 @@ const Faq = () => {
       ref2.current.classList.remove('programs-show');
       ref4.current.classList.remove('programs-show');
       ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
       setArrowUp3((prev) => !prev);
       setArrowUp1(false);
       setArrowUp2(false);
       setArrowUp4(false);
       setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
     }
     if (tarket === '4') {
       ref4.current.classList.toggle('programs-show');
@@ -66,23 +98,119 @@ const Faq = () => {
       ref3.current.classList.remove('programs-show');
       ref2.current.classList.remove('programs-show');
       ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
       setArrowUp4((prev) => !prev);
       setArrowUp1(false);
       setArrowUp2(false);
       setArrowUp3(false);
       setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
     }
     if (tarket === '5') {
       ref5.current.classList.toggle('programs-show');
       ref1.current.classList.remove('programs-show');
+      ref2.current.classList.remove('programs-show');
       ref3.current.classList.remove('programs-show');
       ref4.current.classList.remove('programs-show');
-      ref2.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
       setArrowUp5((prev) => !prev);
       setArrowUp1(false);
       setArrowUp2(false);
       setArrowUp3(false);
       setArrowUp4(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
+    }
+    if (tarket === '6') {
+      ref6.current.classList.toggle('programs-show');
+      ref1.current.classList.remove('programs-show');
+      ref2.current.classList.remove('programs-show');
+      ref3.current.classList.remove('programs-show');
+      ref4.current.classList.remove('programs-show');
+      ref5.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
+      setArrowUp6((prev) => !prev);
+      setArrowUp1(false);
+      setArrowUp2(false);
+      setArrowUp3(false);
+      setArrowUp4(false);
+      setArrowUp5(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
+    }
+    if (tarket === '7') {
+      ref7.current.classList.toggle('programs-show');
+      ref1.current.classList.remove('programs-show');
+      ref2.current.classList.remove('programs-show');
+      ref3.current.classList.remove('programs-show');
+      ref4.current.classList.remove('programs-show');
+      ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
+      setArrowUp7((prev) => !prev);
+      setArrowUp1(false);
+      setArrowUp2(false);
+      setArrowUp3(false);
+      setArrowUp4(false);
+      setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp8(false);
+      setArrowUp9(false);
+    }
+    if (tarket === '8') {
+      ref8.current.classList.toggle('programs-show');
+      ref1.current.classList.remove('programs-show');
+      ref2.current.classList.remove('programs-show');
+      ref3.current.classList.remove('programs-show');
+      ref4.current.classList.remove('programs-show');
+      ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref9.current.classList.remove('programs-show');
+      setArrowUp8((prev) => !prev);
+      setArrowUp1(false);
+      setArrowUp2(false);
+      setArrowUp3(false);
+      setArrowUp4(false);
+      setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp9(false);
+    }
+    if (tarket === '9') {
+      ref9.current.classList.toggle('programs-show');
+      ref1.current.classList.remove('programs-show');
+      ref2.current.classList.remove('programs-show');
+      ref3.current.classList.remove('programs-show');
+      ref4.current.classList.remove('programs-show');
+      ref5.current.classList.remove('programs-show');
+      ref6.current.classList.remove('programs-show');
+      ref7.current.classList.remove('programs-show');
+      ref8.current.classList.remove('programs-show');
+      setArrowUp9((prev) => !prev);
+      setArrowUp1(false);
+      setArrowUp2(false);
+      setArrowUp3(false);
+      setArrowUp4(false);
+      setArrowUp5(false);
+      setArrowUp6(false);
+      setArrowUp7(false);
+      setArrowUp8(false);
     }
   };
   useEffect(() => {
@@ -119,7 +247,7 @@ const Faq = () => {
         >
           <div className="flex justify-between items-center max-w-[1000px]">
             <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
-              Would i have to pay to use this website?
+              Am I the right fit for this program?
             </span>
             <span>
               {arrowUp1 ? (
@@ -131,9 +259,11 @@ const Faq = () => {
           </div>
           <div ref={ref1} className="programs-hide mt-6">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              debitis eius aliquid vero sapiente corrupti rerum perspiciatis
-              maxime excepturi natus!
+              The department welcomes applications from students of diverse
+              backgrounds. The ideal applicant is passionate about the built
+              environment, fascinated with human, spatial and natural
+              interactions within urban environments; and interested in
+              reshaping the world, one square metre at a time.
             </h3>
           </div>
         </div>
@@ -144,7 +274,7 @@ const Faq = () => {
         >
           <div className="flex justify-between items-center max-w-[1000px]">
             <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
-              Would i have to pay to use this website?
+              What is the duration of URP Programs?
             </span>
             <span>
               {arrowUp2 ? (
@@ -156,9 +286,8 @@ const Faq = () => {
           </div>
           <div ref={ref2} className="programs-hide mt-6">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              debitis eius aliquid vero sapiente corrupti rerum perspiciatis
-              maxime excepturi natus!
+              The Bachelor's Program runs for five years while the post graduate
+              degree programs vary between two years and six years.
             </h3>
           </div>
         </div>
@@ -169,7 +298,7 @@ const Faq = () => {
         >
           <div className="flex justify-between items-center max-w-[1000px]">
             <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
-              Would i have to pay to use this website?
+              How can I apply to URP programs in the department?
             </span>
             <span>
               {arrowUp3 ? (
@@ -181,9 +310,9 @@ const Faq = () => {
           </div>
           <div ref={ref3} className="programs-hide mt-6">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              debitis eius aliquid vero sapiente corrupti rerum perspiciatis
-              maxime excepturi natus!
+              Applications are made directly to the University of Lagos. Kindly
+              visit (Insert link to undergraduate and postgraduate admissions)
+              for more information.
             </h3>
           </div>
         </div>
@@ -194,7 +323,7 @@ const Faq = () => {
         >
           <div className="flex justify-between items-center max-w-[1000px]">
             <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
-              Would i have to pay to use this website?
+              What background, experience or skills do I need to study URP?
             </span>
             <span>
               {arrowUp4 ? (
@@ -206,9 +335,12 @@ const Faq = () => {
           </div>
           <div ref={ref4} className="programs-hide mt-6">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              debitis eius aliquid vero sapiente corrupti rerum perspiciatis
-              maxime excepturi natus!
+              The multidisciplinary nature of URP means that all experience and
+              background is relevant. Moreso, our department is committed to
+              fostering an academic environment which enhances skills
+              development and knowledge sharing. Hence, our faculty and students
+              are on ground to provide you with all training you will need in
+              your course duration.
             </h3>
           </div>
         </div>
@@ -219,7 +351,7 @@ const Faq = () => {
         >
           <div className="flex justify-between items-center max-w-[1000px]">
             <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
-              Would i have to pay to use this website?
+              What financial aid options are available for URP students?
             </span>
             <span>
               {arrowUp5 ? (
@@ -231,9 +363,116 @@ const Faq = () => {
           </div>
           <div ref={ref5} className="programs-hide mt-6">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              debitis eius aliquid vero sapiente corrupti rerum perspiciatis
-              maxime excepturi natus!
+              Currently, the department does not offer financial aid. However,
+              we provide support to our students who seek external funding
+              opportunities.
+            </h3>
+          </div>
+        </div>
+        <div
+          className="py-2 px-4 border-b-[1px] border-[#1B1B1B]"
+          role="button"
+          onClick={() => onDropdownClicked('6')}
+        >
+          <div className="flex justify-between items-center max-w-[1000px]">
+            <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
+              How can I connect with the department faculty?
+            </span>
+            <span>
+              {arrowUp6 ? (
+                <KeyboardArrowUpOutlinedIcon htmlColor="#5b5756" />
+              ) : (
+                <KeyboardArrowDownOutlinedIcon htmlColor="#5b5756" />
+              )}
+            </span>
+          </div>
+          <div ref={ref6} className="programs-hide mt-6">
+            <h3>
+              Our faculty are very open to interacting and partnering with
+              prospective students, researchers, partners, and interested
+              members of the public. You can connect directly with our faculty
+              (Link the staff page) or fill out an enquiry form (Link the
+              contact us page) and we will get back to you.
+            </h3>
+          </div>
+        </div>
+        <div
+          className="py-2 px-4 border-b-[1px] border-[#1B1B1B]"
+          role="button"
+          onClick={() => onDropdownClicked('7')}
+        >
+          <div className="flex justify-between items-center max-w-[1000px]">
+            <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
+              What are the job placement opportunities for graduates from the
+              department?
+            </span>
+            <span>
+              {arrowUp7 ? (
+                <KeyboardArrowUpOutlinedIcon htmlColor="#5b5756" />
+              ) : (
+                <KeyboardArrowDownOutlinedIcon htmlColor="#5b5756" />
+              )}
+            </span>
+          </div>
+          <div ref={ref7} className="programs-hide mt-6">
+            <h3>
+              Our graduates are well sought after in the urban planning sector
+              home and abroad, with notable alumni working in Town Planning
+              Offices, Government MDAs, Policy Making, International
+              Organizations, etc.
+            </h3>
+          </div>
+        </div>
+        <div
+          className="py-2 px-4 border-b-[1px] border-[#1B1B1B]"
+          role="button"
+          onClick={() => onDropdownClicked('8')}
+        >
+          <div className="flex justify-between items-center max-w-[1000px]">
+            <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
+              Where can I find the academic calendar and important dates?
+            </span>
+            <span>
+              {arrowUp8 ? (
+                <KeyboardArrowUpOutlinedIcon htmlColor="#5b5756" />
+              ) : (
+                <KeyboardArrowDownOutlinedIcon htmlColor="#5b5756" />
+              )}
+            </span>
+          </div>
+          <div ref={ref8} className="programs-hide mt-6">
+            <h3>
+              The Academic Calendar is published by the University. Please click
+              here to download (or view?, whichever is preferable) the 2023/2024
+              Academic Calendar.
+            </h3>
+          </div>
+        </div>
+        <div
+          className="py-2 px-4 border-b-[1px] border-[#1B1B1B]"
+          role="button"
+          onClick={() => onDropdownClicked('9')}
+        >
+          <div className="flex justify-between items-center max-w-[1000px]">
+            <span className="text-[12px] lg:text-[20px] text-[#1B1B1B]">
+              What extracurricular activities are available within the
+              department?
+            </span>
+            <span>
+              {arrowUp9 ? (
+                <KeyboardArrowUpOutlinedIcon htmlColor="#5b5756" />
+              ) : (
+                <KeyboardArrowDownOutlinedIcon htmlColor="#5b5756" />
+              )}
+            </span>
+          </div>
+          <div ref={ref9} className="programs-hide mt-6">
+            <h3>
+              The department regularly offers field trips under several courses,
+              offering the students the opportunity to explore and travel while
+              gaining course credit. Also, the Urban and Regional Planning
+              Students Association plans extracurricular events every semester
+              for students to connect.
             </h3>
           </div>
         </div>
