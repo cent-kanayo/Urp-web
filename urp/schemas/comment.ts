746bc223531242f62153e1178d4b3c2ad80d6640
cent kanayo <centkanayo@gmail.com>
2024-04-01 10:34:13 +0100

    Refactoring folder structure

---
+++ urp/schemas/comment.ts
@@ -0,0 +1,24 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'message',
+      title: 'Comment',
+      type: 'string',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+  ],
+})
--- urp/schemas/index.ts
+++ urp/schemas/index.ts
@@ -1,5 +1,6 @@
 import blockContent from './blockContent'
 import post from './post'
 import studentOfTheMonth from './studentOfTheMonth'
+import comment from './comment'
 
-export const schemaTypes = [post, blockContent, studentOfTheMonth]
+export const schemaTypes = [post, blockContent, studentOfTheMonth, comment]
